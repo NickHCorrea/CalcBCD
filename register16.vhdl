fdaaad1c58794561cbc32a6e398d66f2c89ea92e
Acegh4 <31373559+Acegh4@users.noreply.github.com>
2021-06-05 19:10:35 -0300

    Update register16.vhdl

--- register16.vhdl
+++ register16.vhdl
@@ -2,12 +2,12 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
 ENTITY register16 IS PORT(
-    d   	  : IN STD_LOGIC_VECTOR(15 DOWNTO 0);
-    ld  	  : IN STD_LOGIC; -- load/enable.
-    clr 	  : IN STD_LOGIC; -- async. clear.
-    clk 	  : IN STD_LOGIC; -- clock.
-    q   	  : OUT STD_LOGIC_VECTOR(15 DOWNTO 0); -- output
-	 flag_ld: OUT STD_LOGIC := '0'
+    d      : IN STD_LOGIC_VECTOR(15 DOWNTO 0); -- Entrada
+    ld     : IN STD_LOGIC; -- Load
+    clr    : IN STD_LOGIC; -- Clear assíncrono
+    clk    : IN STD_LOGIC; -- Clock
+    q      : OUT STD_LOGIC_VECTOR(15 DOWNTO 0); -- Saída
+    flag_ld: OUT STD_LOGIC := '0' -- Flag de Load
 );
 END register16;
 
@@ -17,12 +17,12 @@ BEGIN
     process(clk, clr)
     begin
         if clr = '1' then
-            q <= x"0000";
-				flag_ld <= '0';
+            q <= x"0000"; -- Insire um vetor de 16 bits com valores em 0
+	    flag_ld <= '0'; 
         elsif rising_edge(clk) then
             if ld = '1' then
                 q <= d;
-					 flag_ld <= '1';
+		flag_ld <= '1'; 
             end if;
         end if;
     end process;
