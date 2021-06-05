a86eeda6fe33aa4a95e1432385f2140d87d73404
Acegh4 <31373559+Acegh4@users.noreply.github.com>
2021-06-05 19:43:52 -0300

    Update calcBCD.vhdl

--- calcBCD.vhdl
+++ calcBCD.vhdl
@@ -2,10 +2,9 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
 entity calcBCD is
-	Port(
-			A,B	: in STD_LOGIC_VECTOR( 15 downto 0);
-			op		: in STD_LOGIC_VECTOR(0 downto 0);
-			Saida : out STD_LOGIC_VECTOR( 31 downto 0));
+	Port(A,B  : in STD_LOGIC_VECTOR(15 downto 0);
+	     op	  : in STD_LOGIC_VECTOR(0 downto 0);
+	     Saida: out STD_LOGIC_VECTOR(31 downto 0));
 end calcBCD;
 
 architecture Behavioral of calcBCD is
@@ -15,16 +14,16 @@ signal resSoma32, resMult: STD_LOGIC_VECTOR( 31 downto 0);
 signal carrySoma: STD_LOGIC;
 
 component soma4Algarismos is 
-      Port ( A4: in STD_LOGIC_VECTOR(15 downto 0);
-				 B4: in STD_LOGIC_VECTOR(15 downto 0);
-				 S4: out STD_LOGIC_VECTOR(15 downto 0);
-				 Cout4: out STD_LOGIC);
+      Port (A4   : in STD_LOGIC_VECTOR(15 downto 0);
+	    B4   : in STD_LOGIC_VECTOR(15 downto 0);
+	    S4   : out STD_LOGIC_VECTOR(15 downto 0);
+	    Cout4: out STD_LOGIC);
 end component;
 
 component mult4x4Algarismos is
     Port(entrada_A: in STD_LOGIC_VECTOR(15 downto 0);
          entrada_b: in STD_LOGIC_VECTOR(15 downto 0);
-         saida:     out STD_LOGIC_VECTOR(31 downto 0));
+         saida    : out STD_LOGIC_VECTOR(31 downto 0));
 end component;
 
 begin
@@ -47,4 +46,5 @@ begin
 	Saida <= resMult;
 	end if;
 	end process;
+		
 end Behavioral;
